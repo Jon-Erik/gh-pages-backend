e109d452fdfb0e5b9dfc86a7e031b72fb0918aa8
Jon-Erik Chandler <jchandler@avantia-inc.com>
2025-05-06 12:48:43 -0400

    Reactivate recaptcha function

--- services/recaptcha.js
+++ services/recaptcha.js
@@ -5,7 +5,6 @@ const {
 } = process.env
 
 async function validateRecaptcha(captchaToken) {
-    return true
     try {
         const { data } = await axios.post(`${RECAPTCHA_VERIFY_URL}?secret=${RECAPTCHA_SECRET_KEY}&response=${captchaToken}`)
         // console.log({data})
